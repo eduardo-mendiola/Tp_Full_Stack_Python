7669eb2232adfa4f6fd376fae61c5c8d98525c01
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2023-09-27 23:20:29 -0300

    Implementacion de nav-burger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const elementosMenu = document.querySelectorAll('.menu__link');
+const elementosMenu = document.querySelectorAll('.menu-desktop__link');
 const opciones = document.querySelectorAll('.options');
 
 function mostrarOpciones(id) {
@@ -32,6 +32,54 @@ opciones.forEach((opcion) => {
 
 
 
+
+
+
+
+
+
+var theToggle = document.querySelector('.menu-toggle');
+
+// based on Todd Motto functions
+// https://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
 
 
 
